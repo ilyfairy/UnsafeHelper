493f127aafcf3671810595beeefa4f832d88a7a1
小仙女 <me@ilyfairy.com>
2022-08-05 21:25:40 +0800

    新增两个方法

--- UnsafeHelper/UnsafeHelper.cs
+++ UnsafeHelper/UnsafeHelper.cs
@@ -11,6 +11,7 @@ namespace IlyfairyLib.Unsafe
         private static readonly GetRawDataDelegate GetRawData;
 
         private delegate ref byte GetRawDataDelegate(object obj);
+        private delegate void MemoryCopyDelegate(ref byte dest, ref byte src, UIntPtr len);
 
         static UnsafeHelper()
         {
@@ -25,7 +26,7 @@ namespace IlyfairyLib.Unsafe
         /// <typeparam name="T"></typeparam>
         /// <param name="obj"></param>
         /// <returns></returns>
-        public static T? CloneEmptyObject<T>(T? obj)
+        public static T CloneEmptyObject<T>(T obj)
         {
             return (T?)AllocateUninitializedClone(obj);
         }
@@ -72,6 +73,36 @@ namespace IlyfairyLib.Unsafe
             {
                 return new Span<byte>(p, (int)GetObjectDataSize(obj));
             }
+        }
+        /// <summary>
+        /// 将src的内存复制到dest
+        /// </summary>
+        /// <param name="dest"></param>
+        /// <param name="src"></param>
+        /// <param name="len">复制的长度</param>
+        public static void MemoryCopy(ref byte dest, ref byte src, UIntPtr len)
+        {
+            var methods = typeof(Buffer).GetMethods(BindingFlags.NonPublic | BindingFlags.Static);
+            MethodInfo[]? r = methods.Where(v => v.Name == "Memmove").ToArray();
+
+            var method = r.FirstOrDefault((m) => m.GetGenericArguments().Length == 0);
+            var func = method.CreateDelegate<MemoryCopyDelegate>();
+            func(ref dest, ref src, len);
+        }
+        /// <summary>
+        /// 克隆一个对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static T Clone<T>(T obj)
+        {
+            var newObj = UnsafeHelper.CloneEmptyObject(obj); //克隆对象
+            var size = UnsafeHelper.GetObjectDataSize(obj); //长度
+            ref byte oldRef = ref UnsafeHelper.GetObjectRawData(obj); //旧的地址引用
+            ref byte newRef = ref UnsafeHelper.GetObjectRawData(newObj); //新的地址引用
+            MemoryCopy(ref newRef, ref oldRef, size);
+            return newObj;
         }
     }
 
