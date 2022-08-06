7e566e415795955d87c8a265ba21057c2fabc824
小仙女 <me@ilyfairy.com>
2022-08-06 13:21:54 +0800

    提交

--- UnsafeHelper/UnsafeHelper.cs
+++ UnsafeHelper/UnsafeHelper.cs
@@ -1,14 +1,18 @@
 ﻿using System.Reflection;
+using System.Reflection.PortableExecutable;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace IlyfairyLib.Unsafe
 {
     public static class UnsafeHelper
     {
         private static readonly Type RuntimeHelpersType;
+        private static readonly Type BufferType;
         private static readonly Func<object?, object?> AllocateUninitializedClone;
         private static readonly Func<object?, UIntPtr> GetRawObjectDataSize;
         private static readonly GetRawDataDelegate GetRawData;
+        private static readonly MemoryCopyDelegate Memmove;
 
         private delegate ref byte GetRawDataDelegate(object obj);
         private delegate void MemoryCopyDelegate(ref byte dest, ref byte src, UIntPtr len);
@@ -16,9 +20,11 @@ namespace IlyfairyLib.Unsafe
         static UnsafeHelper()
         {
             RuntimeHelpersType = typeof(RuntimeHelpers);
+            BufferType = typeof(Buffer);
             AllocateUninitializedClone = RuntimeHelpersType.GetMethod("AllocateUninitializedClone", BindingFlags.Static | BindingFlags.NonPublic)!.CreateDelegate<Func<object, object>>()!;
             GetRawObjectDataSize = RuntimeHelpersType.GetMethod("GetRawObjectDataSize", BindingFlags.Static | BindingFlags.NonPublic)!.CreateDelegate<Func<object, UIntPtr>>()!;
             GetRawData = RuntimeHelpersType.GetMethod("GetRawData", BindingFlags.Static | BindingFlags.NonPublic)!.CreateDelegate<GetRawDataDelegate>();
+            Memmove = BufferType.GetMethods(BindingFlags.NonPublic | BindingFlags.Static).Where(v => v.Name == "Memmove").FirstOrDefault((m) => m.GetGenericArguments().Length == 0).CreateDelegate<MemoryCopyDelegate>();
         }
         /// <summary>
         /// 克隆至空的对象
@@ -35,7 +41,7 @@ namespace IlyfairyLib.Unsafe
         /// </summary>
         /// <param name="obj"></param>
         /// <returns></returns>
-        public static UIntPtr GetObjectDataSize(object obj)
+        public static UIntPtr GetObjectRawDataSize(object obj)
         {
             return GetRawObjectDataSize(obj);
         }
@@ -53,7 +59,7 @@ namespace IlyfairyLib.Unsafe
         /// </summary>
         /// <param name="obj"></param>
         /// <returns></returns>
-        public static unsafe UIntPtr GetObjectAddress(object obj)
+        public static unsafe UIntPtr GetObjectRawDataAddress(object obj)
         {
             ref byte first = ref GetRawData(obj);
             fixed (void* p = &first)
@@ -66,12 +72,12 @@ namespace IlyfairyLib.Unsafe
         /// </summary>
         /// <param name="obj"></param>
         /// <returns></returns>
-        public static unsafe Span<byte> GetObjectRawDataAsSpan(object obj)
+        public static unsafe Span<T> GetObjectRawDataAsSpan<T>(object obj) where T : unmanaged
         {
             ref byte first = ref GetRawData(obj);
             fixed (void* p = &first)
             {
-                return new Span<byte>(p, (int)GetObjectDataSize(obj));
+                return new Span<T>(p, (int)GetObjectRawDataSize(obj) / sizeof(T));
             }
         }
         /// <summary>
@@ -82,12 +88,7 @@ namespace IlyfairyLib.Unsafe
         /// <param name="len">复制的长度</param>
         public static void MemoryCopy(ref byte dest, ref byte src, UIntPtr len)
         {
-            var methods = typeof(Buffer).GetMethods(BindingFlags.NonPublic | BindingFlags.Static);
-            MethodInfo[]? r = methods.Where(v => v.Name == "Memmove").ToArray();
-
-            var method = r.FirstOrDefault((m) => m.GetGenericArguments().Length == 0);
-            var func = method.CreateDelegate<MemoryCopyDelegate>();
-            func(ref dest, ref src, len);
+            Memmove(ref dest, ref src, len);
         }
         /// <summary>
         /// 克隆一个对象
@@ -97,12 +98,98 @@ namespace IlyfairyLib.Unsafe
         /// <returns></returns>
         public static T Clone<T>(T obj)
         {
-            var newObj = UnsafeHelper.CloneEmptyObject(obj); //克隆对象
-            var size = UnsafeHelper.GetObjectDataSize(obj); //长度
+            T newObj = UnsafeHelper.CloneEmptyObject(obj); //克隆对象
+            UIntPtr size = UnsafeHelper.GetObjectRawDataSize(obj); //长度
             ref byte oldRef = ref UnsafeHelper.GetObjectRawData(obj); //旧的地址引用
             ref byte newRef = ref UnsafeHelper.GetObjectRawData(newObj); //新的地址引用
             MemoryCopy(ref newRef, ref oldRef, size);
             return newObj;
+        }
+        /// <summary>
+        /// 获取对象句柄(对象头)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static unsafe IntPtr GetObjectHandle(object obj)
+        {
+            ref byte objRawDataPtr = ref GetObjectRawData(obj);
+            fixed (void* p = &objRawDataPtr)
+            {
+                var rawData = new IntPtr(p);
+                rawData -= sizeof(IntPtr);
+                return *(IntPtr*)(rawData);
+            }
+        }
+        /// <summary>
+        /// 获取对象句柄
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static IntPtr GetObjectHandle(Type type)
+        {
+            return type.TypeHandle.Value;
+        }
+        /// <summary>
+        /// 输出大写地址字符串, 不包含0x
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static unsafe string ToAddress(this UIntPtr p)
+        {
+            return p.ToString("X").PadLeft(sizeof(UIntPtr) * 2, '0');
+        }
+        /// <summary>
+        /// 输出大写地址字符串, 不包含0x
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static unsafe string ToAddress(this IntPtr p)
+        {
+            return p.ToString("X").PadLeft(sizeof(UIntPtr) * 2, '0');
+        }
+        /// <summary>
+        /// 修改对象Handle
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="handle"></param>
+        public static unsafe void ChangeObjectHandle(object obj, IntPtr handle)
+        {
+            ref byte objRawDataPtr = ref GetObjectRawData(obj);
+            fixed (void* p = &objRawDataPtr)
+            {
+                var rawData = new IntPtr(p);
+                rawData -= sizeof(IntPtr);
+                *(IntPtr*)(rawData) = handle;
+            }
+        }
+        /// <summary>
+        /// 创建一个空的对象
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="size">大小(包含Handle)</param>
+        /// <returns></returns>
+        public static unsafe object? CreateEmptyObject(Type type, int size)
+        {
+            object[] obj = new object[1];
+            IntPtr p = RuntimeHelpers.AllocateTypeAssociatedMemory(type, size);
+            var span = GetObjectRawDataAsSpan<IntPtr>(obj);
+            *((IntPtr*)p) = GetObjectHandle(type);
+            span[1] = p;
+            try
+            {
+                var t = obj[0].GetType();
+                if (obj[0] == null)
+                {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return obj[0];
         }
     }
 
